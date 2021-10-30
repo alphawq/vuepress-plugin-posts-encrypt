1e588791e0ad79e4a23a5a8a32b895ec7310b40e
Aphasia <alphawq@foxmail.com>
2021-10-30 20:13:35 +0800

    fix: route ends with .html

--- packages/plugin/src/options/generated.ts
+++ packages/plugin/src/options/generated.ts
@@ -5,7 +5,8 @@ import { removedir } from '../utils'
 
 export default (options: Options, ctx: Context, genCiphertext, route: string) => {
   return async (pagePaths: string[]): Promise<void> => {
-    const genPath = path.join(ctx.outDir, `${route}.html`)
+    const _route: string = /\.html$/.test(route) ? route : `${route}.html`
+    const genPath = path.join(ctx.outDir, _route)
     genCiphertext()(genPath, void 0, removedir)
   }
 }
